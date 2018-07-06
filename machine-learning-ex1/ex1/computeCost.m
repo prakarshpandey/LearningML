86786ae3cc6f11d9960eee47d1bac37fa8f5f911
prakarshpandey <ppandey@g.hmc.edu>
2018-07-05 21:09:51 -0700

    finish linear regression for one variable

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -6,7 +6,7 @@ function J = computeCost(X, y, theta)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 
 % ====================== YOUR CODE HERE ======================
@@ -14,9 +14,9 @@ J = 0;
 %               You should set J to the cost.
 
 
-
-
-
+for i = 1:m
+  J += (theta' * X(i, :)' - y(i)) ^ 2 / (2 * m)
+endfor
 % =========================================================================
 
 end
